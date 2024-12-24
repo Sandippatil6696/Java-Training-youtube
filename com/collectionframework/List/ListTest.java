25cc2a22cf2757b1b1881473d307038c1c90e334
Sandip Patil <96sandypatil@gmail.com>
2024-12-24 20:23:47 +0530

    Listtest get modified

--- com/collectionframework/List/ListTest.java
+++ com/collectionframework/List/ListTest.java
@@ -1,5 +1,19 @@
 package com.collectionframework.List;
 
-public class ListTest {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListTest {
+        public static void main(String[] args) {
+
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(2);
+            list.add(10);
+            list.add(50);
+
+            for (Integer number : list) {
+                System.out.print(number + " ");
+            }
+            
+        }
 }

19fc2ced7fe22068a932a4f23d5054640008a3fb
Sandip Patil <96sandypatil@gmail.com>
2024-12-25 19:11:27 +0530

    linkedlist practice example added

---
+++ com/collectionframework/linklist/LinkListTest.java
@@ -0,0 +1,21 @@
+package com.collectionframework.linklist;
+
+import java.util.LinkedList;
+
+public class LinkListTest {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(12);
+        list.add(14);
+        list.add(18);
+        list.add(154);
+        list.add(120);
+
+        for (Integer list2 : list) {
+            System.out.print(list2 + " ");   
+        }
+    }
+
+
+}
